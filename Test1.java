8d962b9fa81591cd57362ac133d0e0028151b9db
yumaon <110388364+yumaon@users.noreply.github.com>
2023-04-20 00:01:43 +0000

    java 復習

--- Test1.java
+++ Test1.java
@@ -1,13 +1,33 @@
+// import java.util.ArrayList;
+
+// public class Test1 {
+//   public static void main(String...args) {
+//     ArrayList<String> fruits = new ArrayList<>();
+
+//     fruits.add("apple");
+//     fruits.add("orange");
+//     fruits.add("lemon");
+
+//     fruits.forEach(fruit -> System.out.println(fruit));
+//   }
+// }
+
+import java.util.*;
 import java.util.ArrayList;
 
 public class Test1 {
   public static void main(String...args) {
-    ArrayList<String> fruits = new ArrayList<>();
-
-    fruits.add("apple");
-    fruits.add("orange");
-    fruits.add("lemon");
-
-    fruits.forEach(fruit -> System.out.println(fruit));
+    Scanner scan = new Scanner(System.in);
+    ArrayList<String> names = new ArrayList<>();
+    int num = scan.nextInt();
+    String name;
+    for(int i = 0; i < num; i++) {
+      name = scan.next();
+      names.add(name);
+    }
+    names.forEach(n -> {
+      String nUpper = n.toUpperCase();
+      System.out.println(nUpper);
+    });
   }
 }
