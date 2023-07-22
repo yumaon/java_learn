a71381dd0516849ba7eb6458c9fe1fc720efe5d6
yumaon <110388364+yumaon@users.noreply.github.com>
2023-07-22 20:05:31 +0900

    Scanner練習

---
+++ lesson/ScannerLesson.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ScannerLesson {
+  public static void main(String...args) {
+    Scanner sc = new Scanner(System.in);
+    // String data = sc.next();
+    // System.out.println("hello" + data);
+    int data = sc.nextInt();
+    System.out.println(data + 2);
+  }
+}
