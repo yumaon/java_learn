3ac7d52357539ff19ff50a4482ea62f2dd8797c1
yumaon <110388364+yumaon@users.noreply.github.com>
2023-07-22 19:55:15 +0900

    for文の練習

---
+++ lesson/ForLesson.java
@@ -0,0 +1,9 @@
+public class ForLesson {
+  public static void main(String...args) {
+    int i;
+    for(i = 0; i <= 4; i++) {
+      System.out.println("hello" + i);
+    }
+    System.out.println("last" + i);
+  }
+}
