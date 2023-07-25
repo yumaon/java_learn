7946e64a64ff54f8f8f3ce668fd128b652a0835d
yumaon <110388364+yumaon@users.noreply.github.com>
2023-07-25 22:46:53 +0900

    skill check

---
+++ lesson/D191.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class D191 {
+  public static void main(String...args) {
+    Scanner sc = new Scanner(System.in);
+    double data = sc.nextDouble();
+    if(37.0 <= data) {
+      System.out.println("NG");
+    } else {
+      System.out.println("OK");
+    }
+  }
+}
